737b2a1e7209ab9dddbb8e7db3f330d41aa1bda0
kir_7 <kkireeti1@gmail.com>
2026-06-18 22:25:15 +0530

    baseline_fedamp: fedamp strategy

--- baselines/fedamp/fedamp/server_app.py
+++ baselines/fedamp/fedamp/server_app.py
@@ -8,6 +8,10 @@ from flwr.serverapp.strategy import FedAvg
 from fedamp.strategy import FedAMP
 from fedamp.model import Net
 
+import json
+import os
+
+
 # Create ServerApp
 app = ServerApp()
 
@@ -22,12 +26,16 @@ def main(grid: Grid, context: Context) -> None:
     # Load global model
     global_model = Net()
     arrays = ArrayRecord(global_model.state_dict())
-
+    alphaK = float(context.run_config['alphaK'])
+    sigma = float(context.run_config['sigma'])
+ 
     # Initialize FedAMP strategy
     strategy = FedAMP(
         fraction_train=fraction_train,
         fraction_evaluate=1.0,
         min_available_nodes=2,
+        alphaK=alphaK,
+        sigma=sigma
     )
 
     # Start strategy, run FedAMP for `num_rounds`
@@ -41,3 +49,25 @@ def main(grid: Grid, context: Context) -> None:
     print("\nSaving final model to disk...")
     state_dict = result.arrays.to_torch_state_dict()
     torch.save(state_dict, "final_model.pt")
+
+    train_rounds = sorted(result.train_metrics_clientapp.keys())
+    train_losses = [result.train_metrics_clientapp[r]["train_loss"] for r in train_rounds]
+
+    eval_rounds = sorted(result.evaluate_metrics_clientapp.keys())
+    eval_losses = [result.evaluate_metrics_clientapp[r]["eval_loss"] for r in eval_rounds]
+    eval_accs = [result.evaluate_metrics_clientapp[r]["eval_acc"] for r in eval_rounds]
+
+    history = {
+        "train_rounds":train_rounds, 
+        "train_losses":train_losses, 
+        "eval_rounds":eval_rounds, 
+        "eval_accs":eval_accs, 
+        "eval_losses":eval_losses           
+    }
+
+    os.makedirs(context.run_config['save-dir'], exist_ok=True)
+
+    with open(f"{context.run_config['save-dir']}/{num_rounds}_{fraction_train}.json", "w") as f:
+        json.dump(history, f, indent=4)
+ 
+    return result
--- baselines/fedamp/fedamp/utils.py
+++ baselines/fedamp/fedamp/utils.py
@@ -1,9 +1,46 @@
 """FedAMP: Personalized Cross-Silo Federated Learning on Non-IID Data."""
 
-from flwr.common import ArrayRecord
+from flwr.common import ArrayRecord, Array
+
+import matplotlib.pyplot as plt
 
 def combine_aggregated_arrays(
         aggregated_arrays: ArrayRecord, 
         old_arrays: ArrayRecord, 
-        coef_self: float) -> ArrayRecord:
-    raise NotImplementedError("Not yet implemented")
+        coef_self: float) -> ArrayRecord:        
+    result_np = {}
+    for key, val in aggregated_arrays.items():
+        result_np[key] = (old_arrays[key].numpy() * coef_self) + val.numpy()
+    return ArrayRecord({k: Array(v) for k, v in result_np.items()})
+
+
+def plot_history(history: dict, save_fig_path: str = "fedamp_plot.png"):
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+
+    axs[0].plot(history["train_rounds"], history["train_losses"], 
+                label="Train Loss", marker="o", color="blue")
+    axs[0].plot(history["eval_rounds"], history["eval_losses"], 
+                label="Eval Loss", marker="s", color="orange")
+    axs[0].set_title("Loss over Server Rounds")
+    axs[0].set_xlabel("Server Round")
+    axs[0].set_ylabel("Loss")
+    axs[0].legend()
+    axs[0].grid(True, linestyle="--", alpha=0.7)
+
+    axs[1].plot(history["eval_rounds"], history["eval_accs"], 
+                label="Eval Accuracy", marker="s", color="green")
+    axs[1].set_title("Accuracy over Server Rounds")
+    axs[1].set_xlabel("Server Round")
+    axs[1].set_ylabel("Accuracy")
+    axs[1].legend()
+    axs[1].grid(True, linestyle="--", alpha=0.7)
+
+    plt.tight_layout()
+
+    if save_fig_path:
+        plt.savefig(save_fig_path, dpi=300, bbox_inches="tight")
+        print(f"Plot successfully saved to {save_fig_path}")
+        
+    plt.show()
+
